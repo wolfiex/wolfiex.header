a33a67b14f9dc444d4157e2ab86533263e45e9cb
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-06-27 01:20:09 +0100

    adventure fix

--- Header.svelte
+++ Header.svelte
@@ -1,137 +1,3 @@
-<script>
-  import {onMount} from 'svelte';
-  import {
-    OverflowMenu,
-    OverflowMenuItem,
-    Button,
-    ButtonSet,
-  } from 'carbon-components-svelte';
-  import 'carbon-components-svelte/css/g90.css';
-  import './css/header.css';
-
-  let w;
-  const host = 'danielellisresearch.com';
-  const menu = [
-    // {href:'#', name:''},
-    {href: 'https://showcase.' + host, name: 'Showcase',icon:'collections_bookmark'},
-    {href: 'https://medium.' + host, name: 'Published_Articles',icon:'history_edu'},
-    {href: 'https://research.' + host, name: 'PhD',icon:'school'},
-    {href: 'https://coaching.' + host, name: 'Adventure_Coaching',icon:'rocket_launch'},
-    {href: 'https://art.' + host, name: 'Art',icon:
-'palette'},
-
-    {href: '#contact', name: ' Hire',icon:'mail'}, // cv
-  ];
-
-  onMount(() => {
-    window.onscroll = function () {
-      myFunction();
-    };
-
-    //
-
-    var header = document.getElementById('mainHeader');
-    console.log(header);
-    var sticky = header.offsetTop;
-
-    function myFunction() {
-      console.log(header, window.pageYOffset, sticky, header.classList);
-      if (window.pageYOffset > sticky) {
-        header.classList.add('sticky');
-      } else {
-        header.classList.remove('sticky');
-      }
-      console.log(window.pageYOffset > sticky, header.classList);
-    }
-
-    // make the string html compatible
-    var el = document.getElementById('logo');
-    el.innerText = el.textContent =
-      '[]{} §0d|3a|1n|0[--]i|2e|1l|1 |0e|2l|1l|3i|1s|3[+]';
-    var el = document.getElementById('logo2');
-    // el.innerText = el.textContent = ' §3r|2[++]e|2s|0e|1a|3r|2c|1h '
-
-    //'[contracting]{[--]}  d|2a|1n|3i|2e|2l|1 |1e|2l|0l|3i|1[++]s|1   [ [research]]'
-
-    // '[]{[--]} §0d|3a|2n|1i|2e|0l|0 |0e|1l|0l|2i|1[++]s|3 |1 { }{ }{ }{ }{ }{ }{ }{ }{ }{ }{[ \[research.com]\]}'
-    // '[]{[--]} d|2a|0n|3i|2e|2l|1 |0e|1l|0l|2i|1[++]s|3 |1[[]]'
-
-    //origi
-    // []{[--]}  d|2a|1n|3i|2e|2l|1 |1e|2l|0l|3i|1[++]s|1   [ []]
-  });
-</script>
-
-<svelte:window bind:innerWidth={w} />
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-  />
-</svelte:head>
-<main>
-  <div class="top-container">
-    <slot />
-  </div>
-  <header>
-    <div class="sticky-nav" id="mainHeader">
-      <a id="mobile-nav" class="menu-nav" href="#menu-nav" />
-      <div id="logo" class="logo">
-        <!-- <h4 style="font-size:130% ">Daniel Ellis Research</h4> -->
-      </div>
-      <div id="logo2" class="logo" />
-
-      {#if w > 1100}
-        <nav id="menu">
-          <!-- <a id="goUp" href="#home-slider" <="" a="" /> -->
-          <ul id="menu-nav">
-            {#each menu as link}
-              <li class={window.location.origin == link.href ? 'current' : ''}>
-                <a href={link.href}>{link.name}</a>
-              </li>
-            {/each}
-          </ul>
-        </nav>
-      {:else}
-        <div class="overflow">
-          <OverflowMenu
-            flipped
-            size="xl"
-            iconDescription="View Menu Items"
-            id="overflowbtn"
-          >
-            {#each menu as link}
-              <OverflowMenuItem href={link.href} text={link.name} />
-            {/each}
-            <OverflowMenuItem text="Manage credentials" />
-            <OverflowMenuItem
-              href="https://cloud.ibm.com/docs/api-gateway/"
-              text="API documentation"
-            />
-            <OverflowMenuItem danger text="Delete service" />
-          </OverflowMenu>
-          menu
-        </div>
-        {#if w > 600}
-          <div style="float:right;display:inline-block">
-            {#each menu as link}
-              <Button
-                style="width:auto;"
-                kind="ghost"
-                iconDescription={link.name}
-                href={link.href}
-                ><span class="material-symbols-outlined">
-                  {link.icon||'rocket_launch'}
-                  <!-- rocket_launch -->
-                </span></Button
-              >
-            {/each}
-          </div>
-          <!-- {:else} -->
-        {/if}
-      {/if}
-    </div>
-  </header>
-</main>
 
 <style>
   @font-face {
@@ -192,6 +58,16 @@
     display: block;
   }
 
+  :global(a){
+    color:rgb(214, 214, 214)!important;
+  }
+  :global(a:hover){
+    color:#4856fd!important;
+  }
+  :global(a.bx--btn--ghost:hover){
+    background-color:rgba(30, 28, 33, 0.87)
+  }
+  
   :global(body) {
     margin: auto !important;
     padding-top: 10px;
@@ -269,3 +145,129 @@
     font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48;
   }
 </style>
+
+
+
+<script>
+  import {onMount} from 'svelte';
+  import {
+    OverflowMenu,
+    OverflowMenuItem,
+    Button,
+    ButtonSet,
+  } from 'carbon-components-svelte';
+  import 'carbon-components-svelte/css/g90.css';
+  import './css/header.css';
+
+  let w;
+  const host = 'danielellisresearch.com';
+  const menu = [
+    // {href:'#', name:''},
+    {href: 'https://showcase.' + host, name: 'Showcase',icon:'collections_bookmark'},
+    {href: 'https://medium.' + host, name: 'Published-Articles',icon:'history_edu'},
+    {href: 'https://research.' + host, name: 'Academic',icon:'school',disabled:true},
+    {href: 'https://coaching.' + host, name: 'Adventure-Instrucing',icon:'rocket_launch'},
+    {href: 'https://art.' + host, name: 'Art',icon:
+'palette',disabled:true},
+
+    {href:'mailto:contact@danielellisresearch.com?subject=Website Query"', name: ' Contact',icon:'mail'}, // cv
+  ];
+
+  onMount(() => {
+    window.onscroll = function () {
+      myFunction();
+    };
+
+    //
+
+    var header = document.getElementById('mainHeader');
+    // console.log(header);
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+      // console.log(header, window.pageYOffset, sticky, header.classList);
+      if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+      } else {
+        header.classList.remove('sticky');
+      }
+      // console.log(window.pageYOffset > sticky, header.classList);
+    }
+
+    // make the string html compatible
+    var el = document.getElementById('logo');
+    el.innerText = el.textContent =
+    '[]{[--]}  d|2a|1n|3i|2e|2l|1 |1e|2l|0l|3i|1[++]s|1   [ []]'
+     // '[]{} §0d|3a|1n|0[--]i|2e|1l|1 |0e|2l|1l|3i|1s|3[+]';
+   
+  });
+</script>
+
+<svelte:window bind:innerWidth={w} />
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+  />
+</svelte:head>
+<main>
+  <div class="top-container">
+    <slot />
+  </div>
+  <header>
+    <div class="sticky-nav" id="mainHeader">
+      <a href="https://danielellisresearch.com" >
+      <div id="logo" class="logo">
+      </div>
+    </a>
+
+      {#if w > 1100}
+        <nav id="menu">
+          <ul id="menu-nav">
+            {#each menu as link}
+              <li class={window.location.origin == link.href ? 'current' : ''}>
+
+                <a href={link.href}>{link.name}</a>
+              </li>
+            {/each}
+          </ul>
+
+
+
+        </nav>
+      {:else}
+        <div class="overflow">
+          <OverflowMenu
+            flipped
+            size="xl"
+            iconDescription="View Menu Items"
+            id="overflowbtn"
+          >
+            {#each menu as link}
+              <OverflowMenuItem href={link.href} text={link.name} />
+            {/each}
+          </OverflowMenu>
+          menu
+        </div>
+        {#if w > 660}
+          <div style="float:right;display:inline-block">
+            {#each menu as link}
+              <Button
+                style="width:auto;"
+                kind="ghost"
+                iconDescription={link.name}
+                href={link.href}
+                disabled={link.disabled}
+                ><span class="material-symbols-outlined">
+                  {link.icon||'rocket_launch'}
+                  <!-- rocket_launch -->
+                </span></Button
+              >
+            {/each}
+          </div>
+          <!-- {:else} -->
+        {/if}
+      {/if}
+    </div>
+  </header>
+</main>
