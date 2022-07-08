c74fb9b024e38fa45ddd30985d759b6b6702a4ad
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-07-08 16:29:25 +0100

    main

--- Header.svelte
+++ Header.svelte
@@ -1,154 +1,18 @@
 
 <style>
-  @font-face {
+    @font-face {
     font-family: 'Datalegreya-Dot';
     src: url('/Datalegreya-Dot.otf');
     font-weight: normal;
     font-style: normal;
   }
 
-  :global(.overflow) {
-    font-size: x-small;
-    display: inline-block;
-    margin: auto;
-    border-color: rgb(37, 35, 35);
-    border: 2px;
-    border-style: groove;
-    float: right !important;
-    right: 0;
-    position: relative;
-  }
-  :global(#overflowbtn) {
-    background-color: rgba(173, 190, 190, 0.053);
-    border: 2px;
-    /* border-style: groove; */
-  }
-  .logo {
-    font-family: Datalegreya-Dot;
-    font-size: 130%;
-    -webkit-font-feature-settings: 'kern' on, 'liga' on, 'calt' on;
-    -moz-font-feature-settings: 'kern' on, 'liga' on, 'calt' on;
-    -webkit-font-feature-settings: 'kern' on, 'liga' on, 'calt' on;
-    -ms-font-feature-settings: 'kern' on, 'liga' on, 'calt' on;
-    font-feature-settings: 'kern' on, 'liga' on, 'calt' on;
-    font-variant-ligatures: common-ligatures discretionary-ligatures contextual;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    margin-left: 10px !important;
-    padding-top: 10px !important;
-    padding-bottom: 1px !important;
-    float: left;
-  }
-
-  #logo {
-    font-size: 130%;
-  }
-  #logo2 {
-    vertical-align: text-bottom;
-    left: 0;
-    bottom: 0%;
-    margin-top: auto !important;
-    font-family: Datalegreya-Dot;
-    font-size: 50%;
-  }
-
-  header {
-    top: 0;
-    display: block;
-  }
-
-  :global(a){
-    color:rgb(214, 214, 214)!important;
-  }
-  :global(a:hover){
-    color:#4856fd!important;
-  }
-  :global(a.bx--btn--ghost:hover){
-    background-color:rgba(30, 28, 33, 0.87)
-  }
-  
-  :global(body) {
-    margin: auto !important;
-    padding-top: 10px;
-    padding-bottom: 10px;
-  }
-  :global(.top-container) {
-    /* background-color: #f1f1f1; */
-    /* padding-bottom: 130px; */
-    position: relative;
-    padding: 20px;
-    height: auto;
-    text-align: center;
-    overflow-x: hidden;
-    overflow-y: hidden;
-  }
-
-:global(main){
-  max-width:none;
-}
-  .sticky-nav {
-    left: 0 !important;
-    /* position:absoulte; */
-    margin-left: 0;
-    padding-left: 0;
-    width: 100vw;
-    font-family: Datalegreya-Dot;
-    font-size: 230% !important;
-    /* position: relative; */
-  }
-
-  li {
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    /* -moz-osx-font-smoothing: grayscale; */
-    font-weight: 800;
-  }
-
-  li a {
-    font-size: 0.65em !important;
-    display: inline-block;
-  }
-  /* .content {
-  padding: 16px;
-} */
-
-  :global(.sticky) {
-    position: fixed !important;
-    left: 0;
-    top: 0 !important;
-    /* width: 100%; */
-  }
-
-  .sticky + .content {
-    padding-top: 202px;
-  }
-
-  :global(html) {
-    scroll-behavior: smooth;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    margin: 0 0 15px;
-    color: #fff;
-    font-weight: 300;
-    font-family: open sans, sans-serif;
-    line-height: 1.5em;
-  }
-
-  .material-symbols-outlined {
-    font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48;
-  }
 </style>
 
 
 
 <script>
+  import './header.css'
   import {onMount} from 'svelte';
   import {
     OverflowMenu,
@@ -158,6 +22,24 @@
   } from 'carbon-components-svelte';
   import 'carbon-components-svelte/css/g90.css';
   import './css/header.css';
+
+  
+  // header style
+  export let style = 'transparent'
+
+  console.error(style)
+  switch(style) {
+  case "dark":
+    style = "background: rgb(38, 41, 46);color:rgb(214, 214, 214);backdrop-filter: blur(10px)";
+    break;
+  case "light":
+    style = "background: rgb(254, 254, 254,.7);color:#222;backdrop-filter: blur(10px)";
+    break;
+  case "transparent":
+    style = 'background: rgba(255,255,255,.051);backdrop-filter: blur(10px);color:rgb(214, 214, 214)'
+    break;
+  }
+
 
   let w;
   const host = 'danielellisresearch.com';
@@ -214,29 +96,16 @@
   <div class="top-container">
     <slot />
   </div>
-  <header>
-    <div class="sticky-nav" id="mainHeader">
+  <header >
+    <div class="sticky-nav" id="mainHeader" style = {style}>
       <a href="https://danielellisresearch.com" >
       <div id="logo" class="logo">
       </div>
     </a>
 
-      {#if w > 1100}
-        <nav id="menu">
-          <ul id="menu-nav">
-            {#each menu as link}
-              <li class={window.location.origin == link.href ? 'current' : ''}>
-
-                <a href={link.href}>{link.name}</a>
-              </li>
-            {/each}
-          </ul>
-
-
-
-        </nav>
-      {:else}
-        <div class="overflow">
+     
+   
+        <!-- <div class="overflow">
           <OverflowMenu
             flipped
             size="xl"
@@ -248,7 +117,10 @@
             {/each}
           </OverflowMenu>
           menu
-        </div>
+        </div> -->
+
+
+
         {#if w > 660}
           <div style="float:right;display:inline-block">
             {#each menu as link}
@@ -267,7 +139,7 @@
           </div>
           <!-- {:else} -->
         {/if}
-      {/if}
+
     </div>
   </header>
 </main>
