cf147512c72e5673a37a13dcb853ea6b53d03a0d
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-07-12 12:00:59 +0100

    redo changes

--- Header.svelte
+++ Header.svelte
@@ -1,120 +1,95 @@
-
-<style>
-    @font-face {
-    font-family: 'Datalegreya-Dot';
-    src: url('/Datalegreya-Dot.otf');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  :global(a#nav-toggles){
-    /* background:rgba(0, 136, 255, 0.1);
-     */
-    
-    backdrop-filter: blur(10px);
-    border-left: 3px double rgba(245, 245, 245, 0.226);
-  }
-  :global(a#nav-toggles>span.material-symbols-outlined){
-    /* color: rgb(251, 78, 15)!important;
-     */
-     color:rgb(4, 183, 240)
-  }
-
-  :global(.sticky) {
-    position: fixed !important;
-    display:block!important;
-    z-index: 99999!important;
-    left: 0!important;
-    top: 0 !important;
-    /* filter:invert(1)!important; */
-    /* width: 100%; */
-  }
-</style>
-
-
-
 <script>
-  import './header.css'
+  import './header.css';
   import {onMount} from 'svelte';
-  import {
-    Button,
-    ButtonSet,
-  } from 'carbon-components-svelte';
+  import {Button, ButtonSet} from 'carbon-components-svelte';
   import 'carbon-components-svelte/css/g90.css';
   // import './css/header.css';
-  import Nav from './Nav.svelte'
+  import Nav from './Nav.svelte';
 
-  
   // header style
-  export let style = 'transparent'
-  var fstyle = '#222'
-  var bstyle = 'whitesmoke'
+  export let style = 'transparent';
+  export let loaded = false;
+  let scrollY=0;
+  let header,sticky;
+  
 
 
-  console.error(style)
-  switch(style) {
-  case "dark":
-    bstyle = "background: rgb(38, 41, 46)!important;"
-    fstyle = "color:rgb(214, 214, 214)!important";
-    break;
-  case "light":
-    bstyle = "background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);"
-    fstyle = "color:#222;";
-    break;
-    case "active":
-    bstyle = "background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);margin-top:0em!important"
-    fstyle = "color:#222;";
-    break;
-  case "transparent":
-    bstyle = 'background: rgba(255,255,255,.051);backdrop-filter: blur(5px);'
-    fstyle = "color:rgb(214, 214, 214)"
-    break;
+  var fstyle = '#222';
+  var bstyle = 'whitesmoke';
+
+  console.error(style);
+  switch (style) {
+    case 'dark':
+      bstyle = 'background: rgb(38, 41, 46)!important;';
+      fstyle = 'color:rgb(214, 214, 214)!important';
+      break;
+    case 'light':
+      bstyle =
+        'background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);';
+      fstyle = 'color:#222;';
+      break;
+    case 'active':
+      bstyle =
+        'background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);margin-top:0em!important';
+      fstyle = 'color:#222;';
+      break;
+    case 'transparent':
+      bstyle = 'background: rgba(255,255,255,.02);backdrop-filter: blur(5px);';
+      fstyle = 'color:rgb(214, 214, 214)';
+      break;
   }
-
 
   let w;
   const host = 'danielellisresearch.com';
   const menu = [
     // {href:'#', name:''},
-    {href: 'https://showcase.' + host, name: 'Showcase',icon:'collections_bookmark'},
-    {href: 'https://medium.' + host, name: 'Published-Articles',icon:'history_edu'},
-    {href: 'https://research.' + host, name: 'Academic',icon:'school',disabled:true},
-    {href: 'https://coaching.' + host, name: 'Adventure-Instructing',icon:'rocket_launch'},
-    {href: 'https://art.' + host, name: 'Art',icon:
-'palette',disabled:true},
+    {
+      href: 'https://showcase.' + host,
+      name: 'Showcase',
+      icon: 'collections_bookmark',
+    },
+    {
+      href: 'https://medium.' + host,
+      name: 'Published-Articles',
+      icon: 'history_edu',
+    },
+    {
+      href: 'https://research.' + host,
+      name: 'Academic',
+      icon: 'school',
+      disabled: true,
+    },
+    {
+      href: 'https://coaching.' + host,
+      name: 'Adventure-Instructing',
+      icon: 'rocket_launch',
+    },
+    {href: 'https://art.' + host, name: 'Art', icon: 'palette', disabled: true},
 
-    {href:'mailto:contact@danielellisresearch.com?subject=Website Query"', name: ' Contact',icon:'mail'}, // cv
+    {
+      href: 'mailto:contact@danielellisresearch.com?subject=Website Query"',
+      name: ' Contact',
+      icon: 'mail',
+    }, // cv
   ];
 
+
+
   onMount(() => {
-    window.onscroll = function () {
-      isSticky();
-    };
 
-    //
+    console.log(header)
+    sticky = header.offsetTop;
 
-    var header = document.getElementById('mainHeader');
-    var sticky = header.offsetTop;
-
-    function isSticky() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add('sticky');
-      } else {
-        header.classList.remove('sticky');
-      }
-      console.log(window.pageYOffset > sticky, header.classList,header);
-    }
-
-    // make the string html compatible
     var el = document.getElementById('logo');
-    el.innerText = el.textContent =
-    '[]{[--]}  d|2a|1n|3i|2e|2l|1 |1e|2l|0l|3i|1[++]s|1   [ []]'
-     // '[]{} §0d|3a|1n|0[--]i|2e|1l|1 |0e|2l|1l|3i|1s|3[+]';
-   
+        el.innerText = el.textContent =
+          '[]{[--]}  d|2a|1n|3i|2e|2l|1 |1e|2l|0l|3i|1[++]s|1   [ []]';
+        // '[]{} §0d|3a|1n|0[--]i|2e|1l|1 |0e|2l|1l|3i|1s|3[+]';
   });
+
+
 </script>
 
-<svelte:window bind:innerWidth={w} />
+<svelte:window bind:innerWidth={w} bind:scrollY />
 <svelte:head>
   <link
     rel="stylesheet"
@@ -126,55 +101,80 @@
     <slot />
   </div>
   <danheader>
-    <div class="sticky-nav" id="mainHeader"  style = {'height:3.5em;'+bstyle} >
-      <a href="https://danielellisresearch.com" >
-      <div id="logo" class="logo" style = {fstyle}>
-      </div>
-    </a>
+    <div class="sticky-nav" class:sticky={scrollY>sticky} id="mainHeader" bind:this={header} style={'height:3.5em;' + bstyle}>
+      <a href="https://danielellisresearch.com">
+        <div id="logo" class="logo" style={fstyle} >Daniel Ellis Research</div>
+      </a>
 
-     
-
-
-        {#if w > 660}
-          <div style={"float:right;display:inline-block" + bstyle}>
-            {#each menu as link}
-              <Button
-                style={"width:auto;"+fstyle}
-                kind="ghost"
-                iconDescription={link.name}
-                href={link.href}
-                disabled={link.disabled}
-               
-                ><span class="material-symbols-outlined">
-                  {link.icon||'rocket_launch'}
-                  <!-- rocket_launch -->
-                </span></Button
-              >
-            {/each}
-          
+      {#if w > 660}
+        <div style={'float:right;display:inline-block' + bstyle}>
+          {#each menu as link}
             <Button
-                style="width:auto;"
-                kind="ghost"
-                iconDescription='Toggle Nav'
-                href={'#'}
-                id="nav-toggles" 
-                on:click={function(d) {
-                  document.getElementById('nav-menu').classList.toggle("active");
-                  console.error('ppp',document.getElementById('nav-menu').classList)
-                }}
-                ><span class="material-symbols-outlined">
-                  {'menu'}
-                  <!-- rocket_launch -->
-                </span></Button
-              >
-          </div>
-          
+              style={'width:auto;' + fstyle}
+              kind="ghost"
+              iconDescription={link.name}
+              href={link.href}
+              disabled={link.disabled}
+              ><span class="material-symbols-outlined">
+                {link.icon || 'rocket_launch'}
+                <!-- rocket_launch -->
+              </span></Button
+            >
+          {/each}
 
-          <!-- {:else} -->
-        {/if}
-        
-        <!-- <Nav menu={menu}/> -->
+          <Button
+            style="width:auto;"
+            kind="ghost"
+            iconDescription="Toggle Nav"
+            href={'#'}
+            id="nav-toggles"
+            on:click={function (d) {
+              document.getElementById('nav-menu').classList.toggle('active');
+              console.error(
+                'ppp',
+                document.getElementById('nav-menu').classList
+              );
+            }}
+            ><span class="material-symbols-outlined">
+              {'menu'}
+              <!-- rocket_launch -->
+            </span></Button
+          >
+        </div>
 
+        <!-- {:else} -->
+      {/if}
+
+      <!-- <Nav menu={menu}/> -->
     </div>
   </danheader>
 </main>
+
+<style>
+  @font-face {
+    font-family: 'Datalegreya-Dot';
+    src: url('/Datalegreya-Dot.woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  :global(a#nav-toggles) {
+    backdrop-filter: blur(10px);
+    border-left: 3px double rgba(245, 245, 245, 0.226);
+  }
+  :global(a#nav-toggles > span.material-symbols-outlined) {
+    /* color: rgb(251, 78, 15)!important;
+     */
+    color: rgb(4, 183, 240);
+  }
+
+  :global(.sticky) {
+    position: fixed !important;
+    display: block !important;
+    z-index: 99999 !important;
+    left: 0 !important;
+    top: 0 !important;
+    /* filter:invert(1)!important; */
+    /* width: 100%; */
+  }
+</style>
