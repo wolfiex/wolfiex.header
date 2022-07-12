00fae5087af14eb8f54b2cc7a40e0acd50cfe278
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-07-12 11:10:36 +0100

    errrmmm

--- Header.svelte
+++ Header.svelte
@@ -1,22 +1,34 @@
 
-<svelte:window bind:innerWidth={w} />
-<svelte:head>
-  <!-- <link rel="preload" href="/Datalegreya-Dot.otf" as="font" type="font/otf" crossorigin> -->
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-  />
-</svelte:head>
-
 <style>
-
-@font-face {
+    @font-face {
     font-family: 'Datalegreya-Dot';
-    src: url('/Datalegreya-Dot.woff2');
+    src: url('/Datalegreya-Dot.otf');
     font-weight: normal;
     font-style: normal;
   }
 
+  :global(a#nav-toggles){
+    /* background:rgba(0, 136, 255, 0.1);
+     */
+    
+    backdrop-filter: blur(10px);
+    border-left: 3px double rgba(245, 245, 245, 0.226);
+  }
+  :global(a#nav-toggles>span.material-symbols-outlined){
+    /* color: rgb(251, 78, 15)!important;
+     */
+     color:rgb(4, 183, 240)
+  }
+
+  :global(.sticky) {
+    position: fixed !important;
+    display:block!important;
+    z-index: 99999!important;
+    left: 0!important;
+    top: 0 !important;
+    /* filter:invert(1)!important; */
+    /* width: 100%; */
+  }
 </style>
 
 
@@ -25,31 +37,37 @@
   import './header.css'
   import {onMount} from 'svelte';
   import {
-    OverflowMenu,
-    OverflowMenuItem,
     Button,
     ButtonSet,
   } from 'carbon-components-svelte';
   import 'carbon-components-svelte/css/g90.css';
-  import './css/header.css';
-
-  
+  // import './css/header.css';
+  import Nav from './Nav.svelte'
 
   
   // header style
-  export let style = 'transparent';
-  export let loading=false;
+  export let style = 'transparent'
+  var fstyle = '#222'
+  var bstyle = 'whitesmoke'
+
 
   console.error(style)
   switch(style) {
   case "dark":
-    style = "background: rgb(38, 41, 46);color:rgb(214, 214, 214);backdrop-filter: blur(10px)";
+    bstyle = "background: rgb(38, 41, 46)!important;"
+    fstyle = "color:rgb(214, 214, 214)!important";
     break;
   case "light":
-    style = "background: rgb(254, 254, 254,.7);color:#222;backdrop-filter: blur(10px)";
+    bstyle = "background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);"
+    fstyle = "color:#222;";
+    break;
+    case "active":
+    bstyle = "background:whitesmoke!important;border-top: .2px solid gray;backdrop-filter: blur(10px);margin-top:0em!important"
+    fstyle = "color:#222;";
     break;
   case "transparent":
-    style = 'background: rgba(255,255,255,.011);backdrop-filter: blur(5px);color:rgb(214, 214, 214)'
+    bstyle = 'background: rgba(255,255,255,.051);backdrop-filter: blur(5px);'
+    fstyle = "color:rgb(214, 214, 214)"
     break;
   }
 
@@ -70,41 +88,22 @@
 
   onMount(() => {
     window.onscroll = function () {
-      myFunction();
+      isSticky();
     };
 
     //
 
     var header = document.getElementById('mainHeader');
-    // console.log(header);
     var sticky = header.offsetTop;
 
-    function myFunction() {
-      // console.log(header, window.pageYOffset, sticky, header.classList);
+    function isSticky() {
       if (window.pageYOffset > sticky) {
         header.classList.add('sticky');
       } else {
         header.classList.remove('sticky');
       }
-      // console.log(window.pageYOffset > sticky, header.classList);
+      console.log(window.pageYOffset > sticky, header.classList,header);
     }
-
-
-    document.fonts.ready.then(function () {
-  // alert('All fonts in use by visible text have loaded.');
-  //  alert('DLG loaded? ' + document.fonts.check('1em Datalegreya-Dot'));  // true
-
-  if (document.fonts.check('1em Datalegreya-Dot')){
-    loading = true;
-    console.warn('finished loading fonts')
-
-  }
-});
-
-// document.fonts.onloadingdone = function (fontFaceSetEvent) {
-//    alert('onloadingdone we have ' + fontFaceSetEvent.fontfaces.length + ' font faces loaded');
-// };
-
 
     // make the string html compatible
     var el = document.getElementById('logo');
@@ -115,54 +114,67 @@
   });
 </script>
 
+<svelte:window bind:innerWidth={w} />
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+  />
+</svelte:head>
 <main>
   <div class="top-container">
     <slot />
   </div>
-  <header >
-    <div class="sticky-nav" id="mainHeader" style = {style}>
+  <danheader>
+    <div class="sticky-nav" id="mainHeader"  style = {'height:3.5em;'+bstyle} >
       <a href="https://danielellisresearch.com" >
-      <div id="logo" class="logo"> Daniel Ellis Research
+      <div id="logo" class="logo" style = {fstyle}>
       </div>
     </a>
 
      
-   
-        <!-- <div class="overflow">
-          <OverflowMenu
-            flipped
-            size="xl"
-            iconDescription="View Menu Items"
-            id="overflowbtn"
-          >
-            {#each menu as link}
-              <OverflowMenuItem href={link.href} text={link.name} />
-            {/each}
-          </OverflowMenu>
-          menu
-        </div> -->
-
 
 
         {#if w > 660}
-          <div style="float:right;display:inline-block">
+          <div style={"float:right;display:inline-block" + bstyle}>
             {#each menu as link}
               <Button
-                style="width:auto;"
+                style={"width:auto;"+fstyle}
                 kind="ghost"
                 iconDescription={link.name}
                 href={link.href}
                 disabled={link.disabled}
+               
                 ><span class="material-symbols-outlined">
                   {link.icon||'rocket_launch'}
                   <!-- rocket_launch -->
                 </span></Button
               >
             {/each}
+          
+            <Button
+                style="width:auto;"
+                kind="ghost"
+                iconDescription='Toggle Nav'
+                href={'#'}
+                id="nav-toggles" 
+                on:click={function(d) {
+                  document.getElementById('nav-menu').classList.toggle("active");
+                  console.error('ppp',document.getElementById('nav-menu').classList)
+                }}
+                ><span class="material-symbols-outlined">
+                  {'menu'}
+                  <!-- rocket_launch -->
+                </span></Button
+              >
           </div>
+          
+
           <!-- {:else} -->
         {/if}
+        
+        <!-- <Nav menu={menu}/> -->
 
     </div>
-  </header>
+  </danheader>
 </main>
---
+++ Nav.svelte
@@ -0,0 +1,57 @@
+<script>
+  import {Link} from 'carbon-components-svelte';
+  import {onMount} from 'svelte';
+
+  export let menu = false;
+//   onMount(() => {
+//     console.log(menu);
+//   });
+
+  $: console.log(menu);
+</script>
+
+<nav id="nav-menu">
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2"
+      >
+        <div class="pull">
+          <ul class="top-nav">
+            {#if menu != false}
+              {#each menu as link}
+                <li>
+                  <a href={link.href}>
+                    {link.name}
+                    <span><i class="fa fa-angle-right" /></span>
+                  </a>
+                </li>
+              {/each}
+            {/if}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<style>
+  nav {
+    /* position: absolute; */
+    display: block;
+    width: 100%;
+    font-size: large;
+  }
+  /* nav:parent{all: revert;} */
+
+  :global(#nav-menu:not(.active)) {
+    opacity: 0;
+    display: none;
+  }
+  :global(#nav-menu.active) {
+    /* This section calls the slideInFromLeft animation we defined above */
+    display: inline;
+    opacity: 1;
+    animation: 1s ease-in 0s 1 slideIn;
+  }
+</style>
